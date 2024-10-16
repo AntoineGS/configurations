5e0733acf28e70a5283649c05f492b2a24a69d78
Antoine Gaudreau Simard <antoineg.simard@gmail.com>
2024-10-15 20:27:37 -0400

    feat(nvim): add workaround for live replacements in vscode-neovim

--- Both/Neovim/nvim/lua/options.lua
+++ Both/Neovim/nvim/lua/options.lua
@@ -1,6 +1,9 @@
-require "nvchad.options"
+if not vim.g.vscode then
+  require "nvchad.options"
+end
 
 -- add yours here!
 
--- local o = vim.o
+local o = vim.o
+o.icm = 'split'
 -- o.cursorlineopt ='both' -- to enable cursorline!
