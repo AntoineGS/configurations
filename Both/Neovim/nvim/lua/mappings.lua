03807fab44a9ddddd2285e9ba15e097567e4f71e
Antoine Gaudreau Simard <a.simard@multidev.net>
2024-10-18 09:51:50 -0400

    fix(nvim): resolve navigation issue with vscode-nvim

--- Both/Neovim/nvim/lua/mappings.lua
+++ Both/Neovim/nvim/lua/mappings.lua
@@ -1,14 +1,20 @@
 if not vim.g.vscode then
   require "nvchad.mappings"
+
+  local cinnamon = require("cinnamon")
+  vim.keymap.set("n", "<C-U>", function() cinnamon.scroll("<C-U>zz") end)
+  vim.keymap.set("n", "<C-D>", function() cinnamon.scroll("<C-D>zz") end)
 end
 
-local cinnamon = require("cinnamon")
-vim.keymap.set("n", "<C-U>", function() cinnamon.scroll("<C-U>zz") end)
-vim.keymap.set("n", "<C-D>", function() cinnamon.scroll("<C-D>zz") end)
--- add yours here
+--[[ local map = vim.keymap.set
+map("n", "<leader>y", '"+y')
+map("v", "<leader>y", '"+y')
+map("n", "<leader>yy", '"+yy')
+map("n", "<leader>Y", '"+Y')
+map("n", "<leader>p", '"+p')
+map("n", "<leader>P", '"+P') ]]
 
-local map = vim.keymap.set
+-- map("n", ";", ":", { desc = "CMD enter command mode" })
+-- map("i", "jk", "<ESC>")
+-- map({ "n)", "i", "v" }, "<C-s>", "<cmd> w <cr>")
 
-map("n", ";", ":", { desc = "CMD enter command mode" })
-map("i", "jk", "<ESC>")
--- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
