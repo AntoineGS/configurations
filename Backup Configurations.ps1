208dc53151ca2ad903cf2faf3e7ebcede6aedf47
Antoine Gaudreau Simard <a.simard@multidev.net>
2024-10-18 09:59:06 -0400

    fix(backup): prevent deleting previously copied files when folders are shared

--- Backup Configurations.ps1
+++ Backup Configurations.ps1
@@ -7,8 +7,12 @@ $paths = @{
     VSCode_keybindings = @{ Source = "~\AppData\Roaming\Code\User\keybindings.json"; Destination = ".\Both\VSCode\User\" }
 }
 
+# Some paths may share the same destination so to prevent deleting a previously copied file we do this in two passes
 foreach ($path in $paths.Values) {
     New-Item -ItemType Directory -Force -Path $path.Destination
     Get-ChildItem -Path $path.Destination -Recurse | Remove-Item -Force -Recurse
+}
+
+foreach ($path in $paths.Values) {
     Copy-Item -Path $path.Source -Destination $path.Destination -Recurse -Force
 }
