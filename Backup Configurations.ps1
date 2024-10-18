663fb7e7b1821493700328804276cbd13984d7ca
Antoine Gaudreau Simard <a.simard@multidev.net>
2024-10-18 09:55:51 -0400

    fix(backup): ensure vscode backup is in the User folder too

--- Backup Configurations.ps1
+++ Backup Configurations.ps1
@@ -3,8 +3,8 @@ $paths = @{
     Profile = @{ Source = $PROFILE; Destination = ".\Windows\PowerShell" }
     Neovim = @{ Source = "~\AppData\Local\nvim\"; Destination = ".\Both\Neovim\" }
     IdeaVim = @{ Source = "~\.ideavimrc"; Destination = ".\Both\IntelliJ\" }
-    VSCode_settings = @{ Source = "~\AppData\Roaming\Code\User\settings.json"; Destination = ".\Both\VSCode\" }
-    VSCode_keybindings = @{ Source = "~\AppData\Roaming\Code\User\keybindings.json"; Destination = ".\Both\VSCode\" }
+    VSCode_settings = @{ Source = "~\AppData\Roaming\Code\User\settings.json"; Destination = ".\Both\VSCode\User\" }
+    VSCode_keybindings = @{ Source = "~\AppData\Roaming\Code\User\keybindings.json"; Destination = ".\Both\VSCode\User\" }
 }
 
 foreach ($path in $paths.Values) {
