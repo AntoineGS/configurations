f7528bbb4462c4c2913047d32ae6d8012877c07e
Antoine Gaudreau Simard <antoineg.simard@gmail.com>
2024-10-31 21:05:31 -0400

    feat: add named parameters to restore, set the wsl instance name and wsl user

--- ConfigManager.ps1
+++ ConfigManager.ps1
@@ -1,33 +1,52 @@
-if ($args.Length -eq 0) {
-    Write-Host "An argument is required. Use 'backup' to backup configurations or 'restore' to restore them."
-    exit
-}
-
-$action = $args[0]
-
-if ($action -ne "backup" -and $action -ne "restore") {
-    Write-Host "Invalid argument. Use 'backup' to backup configurations or 'restore' to restore them."
-    exit
-}
+param (
+    [switch]$restore,
+    [string]$wslInstanceName = "Ubuntu",
+    [string]$wslUser = "antoine"
+)
 
 $profileFile = Split-Path -Path $PROFILE -Leaf
 
 # Define source and destination paths
 $paths = @{
-    Profile           = @{ Data = $PROFILE; Backup = ".\Windows\PowerShell\$profileFile" }
-    Neovim            = @{ Data = "~\AppData\Local\nvim\"; Backup = ".\Both\Neovim\nvim" }
-    IdeaVim           = @{ Data = "~\.ideavimrc"; Backup = ".\Both\IntelliJ\.ideavimrc" }
-    VSCodeSettings    = @{ Data = "~\AppData\Roaming\Code\User\settings.json"; Backup = ".\Both\VSCode\User\settings.json" }
-    VSCodeKeybindings = @{ Data = "~\AppData\Roaming\Code\User\keybindings.json"; Backup = ".\Both\VSCode\User\keybindings.json" }
-    WindowsTerminal   = @{ Data = "~\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json"; 
-        Backup                = ".\Windows\WindowsTerminal\settings.json" 
+    Profile           = @{ 
+        Data   = $PROFILE; 
+        Backup = ".\Windows\PowerShell\$profileFile" 
     }
-    Bashrc            = @{ Data = "\\wsl.localhost\Ubuntu\home\antoinegs\.bashrc"; Backup = ".\Linux\Bash\.bashrc" }
-    Starship          = @{ Data = "~\.config\starship.toml"; Backup = ".\Both\Starship\starship.toml" }
-    Warp              = @{ Data = "\\wsl.localhost\Ubuntu\home\antoinegs\.config\warp-terminal\user_preferences.json"; Backup = ".\Linux\Warp\user_preferences.json" }
+    Neovim            = @{ 
+        Data   = "~\AppData\Local\nvim\"; 
+        Backup = ".\Both\Neovim\nvim" 
+    }
+    IdeaVim           = @{ 
+        Data   = "~\.ideavimrc"; 
+        Backup = ".\Both\IntelliJ\.ideavimrc" 
+    }
+    VSCodeSettings    = @{ 
+        Data   = "~\AppData\Roaming\Code\User\settings.json"; 
+        Backup = ".\Both\VSCode\User\settings.json" 
+    }
+    VSCodeKeybindings = @{ 
+        Data   = "~\AppData\Roaming\Code\User\keybindings.json"; 
+        Backup = ".\Both\VSCode\User\keybindings.json" 
+    }
+    WindowsTerminal   = @{ 
+        Data   = "~\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json"; 
+        Backup = ".\Windows\WindowsTerminal\settings.json" 
+    }
+    Bashrc            = @{ 
+        Data   = "\\wsl.localhost\$wslInstanceName\home\$wslUser\.bashrc"; 
+        Backup = ".\Linux\Bash\.bashrc" 
+    }
+    Starship          = @{ 
+        Data   = "~\.config\starship.toml"; 
+        Backup = ".\Both\Starship\starship.toml" 
+    }
+    Warp              = @{ 
+        Data   = "\\wsl.localhost\$wslInstanceName\home\$wslUser\.config\warp-terminal\user_preferences.json"; 
+        Backup = ".\Linux\Warp\user_preferences.json" 
+    }
 }
 
-if ($action -eq "backup") {
+if ($restore) {
     # Some paths may share the same destination so to prevent deleting a previously copied file we do this in two passes
     foreach ($path in $paths.Values) {
         $actualPath = Split-Path -Path $path.Backup
@@ -42,7 +61,7 @@ if ($action -eq "backup") {
         Copy-Item -Path $path.Data -Destination $path.Backup -Recurse -Force
     }
 }
-elseif ($action -eq "restore") {
+else {
     foreach ($path in $paths.Values) {
         if ($path.Data -like "*\") {
             $actualPath = $path.Data
