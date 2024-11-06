dce7ac40c8242aa3d9c28742d9826f0e1fafffea
Antoine Gaudreau Simard <antoineg.simard@gmail.com>
2024-11-05 21:24:51 -0500

    feat(nvim): add cmp priorities

--- Both/Neovim/nvim/lua/plugins/nvim-cmp.lua
+++ Both/Neovim/nvim/lua/plugins/nvim-cmp.lua
@@ -41,12 +41,12 @@ return {
 
   opts = {
     sources = {
-      { name = "copilot" },
-      { name = "nvim_lsp" },
-      { name = "luasnip" },
-      { name = "buffer" },
-      { name = "nvim_lua" },
-      { name = "path" },
+      { name = "luasnip", priority = 1000 },
+      { name = "nvim_lsp", priority = 900 },
+      { name = "nvim_lua", priority = 800 },
+      { name = "buffer", priority = 700 },
+      { name = "copilot", priority = 600 },
+      { name = "path", priority = 500 },
     },
   },
 }
