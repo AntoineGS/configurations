983a6b1bbed5dc5b779f5541dd697bdf71ef70af
Antoine Gaudreau Simard <antoineg.simard@gmail.com>
2025-02-28 21:43:03 -0500

    feat(nvim): migrate to blink.cmp

--- Both/Neovim/nvim/lua/plugins/nvim-lspconfig.lua
+++ Both/Neovim/nvim/lua/plugins/nvim-lspconfig.lua
@@ -1,6 +1,26 @@
 return {
   "neovim/nvim-lspconfig",
-  config = function()
-    require "configs.lspconfig"
+  dependencies = { "saghen/blink.cmp" },
+  opts = {
+    servers = {
+      lua_ls = {},
+      html = {},
+      cssls = {},
+      powershell_es = {},
+      pyright = {},
+      docker_compose_language_service = {},
+      bashls = {},
+      spectral = {},
+      marksman = {},
+    },
+  },
+  config = function(_, opts)
+    local lspconfig = require "lspconfig"
+    for server, config in pairs(opts.servers) do
+      -- passing config.capabilities to blink.cmp merges with the capabilities in your
+      -- `opts[server].capabilities, if you've defined it
+      config.capabilities = require("blink.cmp").get_lsp_capabilities(config.capabilities)
+      lspconfig[server].setup(config)
+    end
   end,
 }
