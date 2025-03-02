4dcc30c51d4767332d6e80ce1e2728338513951c
Antoine Gaudreau Simard <antoineg.simard@gmail.com>
2025-03-01 20:48:11 -0500

    fix(nvim): lsp load issue

--- Both/Neovim/nvim/lua/plugins/nvim-lspconfig.lua
+++ Both/Neovim/nvim/lua/plugins/nvim-lspconfig.lua
@@ -19,7 +19,8 @@ return {
     for server, config in pairs(opts.servers) do
       -- passing config.capabilities to blink.cmp merges with the capabilities in your
       -- `opts[server].capabilities, if you've defined it
-      config.capabilities = require("blink.cmp").get_lsp_capabilities(config.capabilities)
+      -- config.capabilities = require("blink.cmp").get_lsp_capabilities(config.capabilities)
+      config.capabilities = require("blink.cmp").get_lsp_capabilities()
       lspconfig[server].setup(config)
     end
   end,
