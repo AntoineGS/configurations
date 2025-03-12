8c6bd025fa9bbaaaad37d1a03a5ce6ac78dc0eda
Antoine Gaudreau Simard <antoineg.simard@gmail.com>
2025-03-12 14:32:25 -0400

    feat(nvim): temporarily point to my own repo to enable gpt4o until merged

--- Both/Neovim/nvim/lua/plugins/copilot.lua
+++ Both/Neovim/nvim/lua/plugins/copilot.lua
@@ -1,8 +1,12 @@
 return {
-  "zbirenbaum/copilot.lua",
+  -- "zbirenbaum/copilot.lua",
+  "AntoineGS/copilot.lua",
+  branch = "gpt4o",
   cmd = "Copilot",
   event = "InsertEnter",
   config = function()
-    require("copilot").setup {}
+    require("copilot").setup {
+      copilot_model = "gpt-4o-copilot",
+    }
   end,
 }
