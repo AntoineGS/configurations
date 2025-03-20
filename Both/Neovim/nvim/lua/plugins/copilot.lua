9cee5d24da57d8d75f91537b5711f2dacc29f08e
Antoine Gaudreau Simard <antoineg.simard@gmail.com>
2025-03-20 16:05:35 -0400

    feat(nvim): new copilot.lua config

--- Both/Neovim/nvim/lua/plugins/copilot.lua
+++ Both/Neovim/nvim/lua/plugins/copilot.lua
@@ -4,9 +4,51 @@ return {
   branch = "master",
   cmd = "Copilot",
   event = "InsertEnter",
+  -- config = function()
+  --   require("copilot").setup {
+  --     copilot_model = "gpt-4o-copilot",
+  --     -- workspace_fodlers = {
+  --     -- "C:\\Gits\\",
+  --     --   "C:\\Dev\\",
+  --     -- },
+  --     logger = {
+  --       file_log_level = vim.log.levels.TRACE,
+  --       print_log_level = vim.log.levels.WARN,
+  --       log_to_file = true,
+  --       trace_lsp = "verbose",
+  --     },
+  --     filetypes = { ["*"] = true },
+  --   }
+  -- end,
+
   config = function()
     require("copilot").setup {
       copilot_model = "gpt-4o-copilot",
+      logger = {
+        file_log_level = vim.log.levels.TRACE,
+        print_log_level = vim.log.levels.WARN,
+        log_to_file = true,
+        trace_lsp = "verbose",
+      },
+      panel = {
+        enabled = true,
+        keymap = {
+          open = "<M-p>",
+        },
+      },
+      suggestion = {
+        enabled = true,
+        auto_trigger = true,
+        keymap = {
+          accept = "<M-}>",
+          next = "<M-]>",
+          prev = "<M-[>",
+          dismiss = "<C-c>",
+        },
+      },
+      filetypes = {
+        ["*"] = true,
+      },
     }
   end,
 }
