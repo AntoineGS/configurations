17ba9cc7d55130cd367b1a5586c4e40aad07f89f
Antoine Gaudreau Simard <antoineg.simard@gmail.com>
2025-04-08 09:16:38 -0400

    feat(nvim): improve debugging keymaps

--- Both/Neovim/nvim/lua/plugins/nvim-dap.lua
+++ Both/Neovim/nvim/lua/plugins/nvim-dap.lua
@@ -1,7 +1,7 @@
 return {
   "mfussenegger/nvim-dap",
   opts = {}, -- for default options, refer to the configuration section for custom setup.
-  config = function(_, opts)
+  config = function(_, _)
     local dap = require "dap"
     dap.configurations.lua = {
       {
@@ -11,7 +11,19 @@ return {
       },
     }
 
+    dap.configurations.minitest = {
+      {
+        type = "minitest",
+        request = "attach",
+        name = "Attach to running Neovim instance",
+      },
+    }
+
     dap.adapters.nlua = function(callback, config)
+      callback { type = "server", host = config.host or "127.0.0.1", port = config.port or 8086 }
+    end
+
+    dap.adapters.minitest = function(callback, config)
       callback { type = "server", host = config.host or "127.0.0.1", port = config.port or 8086 }
     end
   end,
@@ -20,16 +32,16 @@ return {
   keys = {
     { "<leader>dB", function() require("dap").set_breakpoint(vim.fn.input('Breakpoint condition: ')) end, desc = "Breakpoint Condition" },
     { "<leader>db", function() require("dap").toggle_breakpoint() end, desc = "Toggle Breakpoint" },
-    { "<leader>dc", function() require("dap").continue() end, desc = "Run/Continue" },
+    { "<F9>", function() require("dap").continue() end, desc = "Run/Continue" },
     { "<leader>da", function() require("dap").continue({ before = get_args }) end, desc = "Run with Args" },
     { "<leader>dC", function() require("dap").run_to_cursor() end, desc = "Run to Cursor" },
     { "<leader>dg", function() require("dap").goto_() end, desc = "Go to Line (No Execute)" },
-    { "<leader>di", function() require("dap").step_into() end, desc = "Step Into" },
+    { "<F7>", function() require("dap").step_into() end, desc = "Step Into" },
     { "<leader>dj", function() require("dap").down() end, desc = "Down" },
     { "<leader>dk", function() require("dap").up() end, desc = "Up" },
     { "<leader>dl", function() require("dap").run_last() end, desc = "Run Last" },
     { "<leader>do", function() require("dap").step_out() end, desc = "Step Out" },
-    { "<leader>dO", function() require("dap").step_over() end, desc = "Step Over" },
+    { "<F8>", function() require("dap").step_over() end, desc = "Step Over" },
     { "<leader>dP", function() require("dap").pause() end, desc = "Pause" },
     { "<leader>dr", function() require("dap").repl.toggle() end, desc = "Toggle REPL" },
     { "<leader>ds", function() require("dap").session() end, desc = "Session" },
