5d9fb86ad6867f5aa8d1803ac901beb6a04e0f35
Antoine Gaudreau Simard <antoineg.simard@gmail.com>
2025-04-08 09:17:25 -0400

    feat(nvim): activate mini.test

--- Both/Neovim/nvim/lua/plugins/mini.lua
+++ Both/Neovim/nvim/lua/plugins/mini.lua
@@ -1,4 +1,12 @@
+local wk = require "which-key"
+wk.add {
+  { "<leader>mt", group = "MiniTest" },
+  { "<leader>mtr", "<cmd>lua MiniTest.run()<CR>", desc = "Run" },
+  { "<leader>mtf", "<cmd>lua MiniTest.run_file()<CR>", desc = "Run File" },
+  { "<leader>mtl", "<cmd>lua MiniTest.run_at_location()<CR>", desc = "Run At Location" },
+}
 return {
   "echasnovski/mini.nvim",
   version = false,
+  require("mini.test").setup(),
 }
