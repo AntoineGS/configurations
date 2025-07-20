a8801e80185a5d3027ebfdfafcf89e8ae0e9d273
Antoine Gaudreau Simard <antoineg.simard@gmail.com>
2025-07-20 10:40:29 -0400

    feat(nvim): disable smear-cursor for neovide

--- Both/Neovim/nvim/lua/plugins/smear-cursor.lua
+++ Both/Neovim/nvim/lua/plugins/smear-cursor.lua
@@ -1,11 +1,15 @@
-return {
-  "sphamba/smear-cursor.nvim",
-  lazy = false,
-  opts = { -- Default  Range
-    stiffness = 1, -- 0.6      [0, 1]
-    trailing_stiffness = 0.4, -- 0.4      [0, 1]
-    stiffness_insert_mode = 0.6, -- 0.4      [0, 1]
-    trailing_stiffness_insert_mode = 0.6, -- 0.4      [0, 1]
-    distance_stop_animating = 0.5, -- 0.1      > 0
-  },
-}
+if vim.g.neovide then
+  return {}
+else
+  return {
+    "sphamba/smear-cursor.nvim",
+    lazy = false,
+    opts = { -- Default  Range
+      stiffness = 1, -- 0.6      [0, 1]
+      trailing_stiffness = 0.4, -- 0.4      [0, 1]
+      stiffness_insert_mode = 0.6, -- 0.4      [0, 1]
+      trailing_stiffness_insert_mode = 0.6, -- 0.4      [0, 1]
+      distance_stop_animating = 0.5, -- 0.1      > 0
+    },
+  }
+end
