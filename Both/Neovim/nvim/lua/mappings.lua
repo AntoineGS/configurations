d3fccf38e92086df2c542cdd993e01663880a984
Antoine Gaudreau Simard <antoineg.simard@gmail.com>
2025-10-30 13:57:16 -0400

    feat(nvim): tmux integration and mapping

--- Both/Neovim/nvim/lua/mappings.lua
+++ Both/Neovim/nvim/lua/mappings.lua
@@ -16,10 +16,14 @@ map("i", "<C-l>", "<Right>", { desc = "move right" })
 map("i", "<C-j>", "<Down>", { desc = "move down" })
 map("i", "<C-k>", "<Up>", { desc = "move up" })
 
-map("n", "<C-h>", "<C-w>h", { desc = "switch window left" })
-map("n", "<C-l>", "<C-w>l", { desc = "switch window right" })
-map("n", "<C-j>", "<C-w>j", { desc = "switch window down" })
-map("n", "<C-k>", "<C-w>k", { desc = "switch window up" })
+-- map("n", "<C-h>", "<C-w>h", { desc = "switch window left" })
+-- map("n", "<C-l>", "<C-w>l", { desc = "switch window right" })
+-- map("n", "<C-j>", "<C-w>j", { desc = "switch window down" })
+-- map("n", "<C-k>", "<C-w>k", { desc = "switch window up" })
+map("n", "<C-h>", "<cmd> TmuxNavigateLeft<CR>", { desc = "switch window left" })
+map("n", "<C-l>", "<cmd> TmuxNavigateRight<CR>", { desc = "switch window right" })
+map("n", "<C-j>", "<cmd> TmuxNavigateDown<CR>", { desc = "switch window down" })
+map("n", "<C-k>", "<cmd> TmuxNavigateUp<CR>", { desc = "switch window up" })
 
 map("n", "<Esc>", "<cmd>noh<CR>", { desc = "general clear highlights" })
 
