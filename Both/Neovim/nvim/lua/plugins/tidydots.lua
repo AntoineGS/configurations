de9d2a25511c97473c8a33e15385b186dc11f996
Antoine Gaudreau Simard <antoineg.simard@gmail.com>
2026-02-24 08:48:17 -0500

    feat(nvim): add keybind for tidydots

--- Both/Neovim/nvim/lua/plugins/tidydots.lua
+++ Both/Neovim/nvim/lua/plugins/tidydots.lua
@@ -1,3 +1,9 @@
+local wk = require "which-key"
+
+wk.add {
+  { "<leader>tp", "<cmd>TidydotsPreview<cr>", desc = "Preview Tidydots" },
+}
+
 return {
   dir = "~/gits/tidydots.nvim",
   cmd = "TidydotsPreview",
