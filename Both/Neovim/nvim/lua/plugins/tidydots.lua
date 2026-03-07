27217a1ccb94b4679df9da32e29f40dc7cf182b7
Antoine Gaudreau Simard <antoineg.simard@gmail.com>
2026-03-06 22:03:09 -0500

    fix(nvim): tidydots.nvim on non-dev machines

--- Both/Neovim/nvim/lua/plugins/tidydots.lua
+++ Both/Neovim/nvim/lua/plugins/tidydots.lua
@@ -4,6 +4,19 @@ wk.add {
   { "<leader>tp", "<cmd>TidydotsPreview<cr>", desc = "Preview Tidydots" },
 }
 
+-- if local dir exists use it, otherwise use the one from github
+if vim.fn.isdirectory "~/gits/tidydots.nvim" == 0 then
+  return {
+    "AntoineGS/tidydots.nvim",
+    event = "BufReadPre *.tmpl",
+    opts = {
+      cmd = "tidydots",
+      auto_open = true,
+      debug = true,
+    },
+  }
+end
+
 return {
   dir = "~/gits/tidydots.nvim",
   event = "BufReadPre *.tmpl",
