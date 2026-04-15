e6d87f995933ed546c012d256ab31c1a8753d1bc
Antoine Gaudreau Simard <a.simard@multidev.net>
2026-04-15 15:47:19 -0400

    fix(nvim): tiny inline diag wrapping

--- Both/Neovim/nvim/lua/plugins/tiny-inline-diagnostic.lua
+++ Both/Neovim/nvim/lua/plugins/tiny-inline-diagnostic.lua
@@ -10,6 +10,10 @@ return {
           enabled = true,
         },
         show_diags_only_under_cursor = false,
+        overflow = {
+          mode = "wrap", -- "wrap": split into lines, "none": no truncation, "oneline": keep single line
+          padding = 0, -- Extra characters to trigger wrapping earlier
+        },
       },
     }
   end,
