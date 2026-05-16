ca53009e6573edfe5e408855ee77b9eaacc0241f
Antoine Gaudreau Simard <antoineg.simard@gmail.com>
2026-05-16 18:02:55 -0400

    feat(nvim): expan diffview

--- Both/Neovim/nvim/lua/plugins/diffview.lua
+++ Both/Neovim/nvim/lua/plugins/diffview.lua
@@ -4,10 +4,12 @@ wk.add {
   { "<leader>gc", "<cmd>DiffviewClose<cr>", desc = "DiffviewClose" },
   { "<leader>gt", "<cmd>DiffviewToggle<cr>", desc = "DiffviewToggle" },
   { "<leader>gi", "<cmd>DiffviewFocusFiles<cr>", desc = "DiffviewFocusFiles" },
+  { "<leader>gh", "<cmd>DiffviewFileHistory<cr>", desc = "DiffviewFileHistory (repo)" },
+  { "<leader>gH", "<cmd>DiffviewFileHistory %<cr>", desc = "DiffviewFileHistory (file)" },
 }
 
 return {
   "dlyongemallo/diffview.nvim",
   lazy = true,
-  cmd = { "DiffviewOpen", "DiffviewClose", "DiffviewToggle", "DiffviewFocusFiles" },
+  cmd = { "DiffviewOpen", "DiffviewClose", "DiffviewToggle", "DiffviewFocusFiles", "DiffviewFileHistory" },
 }
