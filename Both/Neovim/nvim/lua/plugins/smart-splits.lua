8ca635c0ca4ccdcbe490caa55c28eefb828f068e
Antoine Gaudreau Simard <a.simard@multidev.net>
2026-05-19 09:53:23 -0400

    feat(nvim): swap vim-tmux-nav to smart-cplits for zellij support

---
+++ Both/Neovim/nvim/lua/plugins/smart-splits.lua
@@ -0,0 +1,9 @@
+return {
+  "mrjones2014/smart-splits.nvim",
+  lazy = false,
+  opts = {
+    -- auto-detects tmux / zellij / wezterm / kitty from $TMUX, $ZELLIJ, etc.
+    multiplexer_integration = nil,
+    at_edge = "wrap",
+  },
+}
