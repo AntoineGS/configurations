c6301a6e22c40911f3c988d0f5d63913e32e86ae
Antoine Gaudreau Simard <antoineg.simard@gmail.com>
2026-07-27 21:42:35 -0400

    style(nvim): format copilot.lua file

--- Both/Neovim/nvim/lua/plugins/copilot.lua
+++ Both/Neovim/nvim/lua/plugins/copilot.lua
@@ -32,71 +32,71 @@ return vim.tbl_deep_extend("force", src, {
   config = function()
     vim.schedule(function()
       require("copilot").setup {
-      -- trace = "verbose",
-      -- copilot_model = "gpt-4o-copilot",
-      logger = logger_conf,
-      -- panel = {
-      --   enabled = true,
-      --   auto_refresh = true,
-      --   keymap = {
-      --     open = false,
-      --   },
-      -- },
-      suggestion = {
-        enabled = true,
-        auto_trigger = true,
-        keymap = {
-          accept = "<C-p>",
-          accept_word = "<M-p>",
-          accept_line = "<M-P>",
-          -- accept = "<TAB>",
-          next = "<M-]>",
-          prev = "<M-[>",
-          -- dismiss = "<C-c>",
+        -- trace = "verbose",
+        -- copilot_model = "gpt-4o-copilot",
+        logger = logger_conf,
+        -- panel = {
+        --   enabled = true,
+        --   auto_refresh = true,
+        --   keymap = {
+        --     open = false,
+        --   },
+        -- },
+        suggestion = {
+          enabled = true,
+          auto_trigger = true,
+          keymap = {
+            accept = "<C-p>",
+            accept_word = "<M-p>",
+            accept_line = "<M-P>",
+            -- accept = "<TAB>",
+            next = "<M-]>",
+            prev = "<M-[>",
+            -- dismiss = "<C-c>",
+          },
         },
-      },
-      nes = {
-        enabled = false,
-        auto_trigger = true,
-        keymap = {
-          -- accept_and_goto = false,
-          -- accept = "<leader>p",
-          accept_and_goto = "<leader>p",
-          accept = false,
-          dismiss = "<Esc>",
+        nes = {
+          enabled = false,
+          auto_trigger = true,
+          keymap = {
+            -- accept_and_goto = false,
+            -- accept = "<leader>p",
+            accept_and_goto = "<leader>p",
+            accept = false,
+            dismiss = "<Esc>",
+          },
         },
-      },
-      filetypes = {
-        ["*"] = true,
-      },
-      should_attach = function(_, bufname)
-        local logger = require "copilot.logger"
+        filetypes = {
+          ["*"] = true,
+        },
+        should_attach = function(_, bufname)
+          local logger = require "copilot.logger"
 
-        if string.match(bufname, ".*copilot-lua\\.log") then
-          logger.debug("not attaching, buffer name is " .. bufname)
-          return false
-        end
+          if string.match(bufname, ".*copilot-lua\\.log") then
+            logger.debug("not attaching, buffer name is " .. bufname)
+            return false
+          end
 
-        if not vim.bo.buflisted then
-          logger.debug "not attaching, buffer is not 'buflisted'"
-          return false
-        end
+          if not vim.bo.buflisted then
+            logger.debug "not attaching, buffer is not 'buflisted'"
+            return false
+          end
 
-        if vim.bo.buftype ~= "" then
-          logger.debug("not attaching, buffer 'buftype' is " .. vim.bo.buftype)
-          return false
-        end
+          if vim.bo.buftype ~= "" then
+            logger.debug("not attaching, buffer 'buftype' is " .. vim.bo.buftype)
+            return false
+          end
 
-        return true
-      end,
-      server = {
-        -- type = "nodejs",
-        -- type = "binary",
-        -- custom_server_filepath = "C:\\Users\\antoi\\AppData\\Local\\nvim-data\\lazy\\copilot.lua\\copilot\\js_2\\language-server.js",
-      },
-      -- auth_provider_url = "https://someurl.com",
-      -- lsp_binary = "C:\\Users\\antoi\\AppData\\Local\\nvim-data\\lazy\\copilot.lua\\copilot\\copilot-language-server.exe",
-    }
+          return true
+        end,
+        server = {
+          -- type = "nodejs",
+          -- type = "binary",
+          -- custom_server_filepath = "C:\\Users\\antoi\\AppData\\Local\\nvim-data\\lazy\\copilot.lua\\copilot\\js_2\\language-server.js",
+        },
+        -- auth_provider_url = "https://someurl.com",
+        -- lsp_binary = "C:\\Users\\antoi\\AppData\\Local\\nvim-data\\lazy\\copilot.lua\\copilot\\copilot-language-server.exe",
+      }
     end)
   end,
 
