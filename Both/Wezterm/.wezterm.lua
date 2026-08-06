7d562acbaffd1967294d1b8d055d917bfcce79b3
Antoine Gaudreau Simard <a.simard@multidev.net>
2026-08-05 22:11:10 -0400

    feat(wezterm): update bindings

--- Both/Wezterm/.wezterm.lua
+++ Both/Wezterm/.wezterm.lua
@@ -103,11 +103,11 @@ config.keys = {
 	},
 
 	-- Misc
-	{ key = "d", mods = "CTRL|SHIFT", action = act.ShowDebugOverlay },
 	{ key = "l", mods = "ALT", action = act.ShowLauncher },
 
 	--- Ignores
 	{ key = "Enter", mods = "ALT", action = act.DisableDefaultAssignment },
+	{ key = "u", mods = "CTRL|SHIFT", action = act.DisableDefaultAssignment },
 	{ key = "LeftArrow", mods = "CTRL|SHIFT|ALT", action = act.DisableDefaultAssignment },
 	{ key = "RightArrow", mods = "CTRL|SHIFT|ALT", action = act.DisableDefaultAssignment },
 	{ key = "UpArrow", mods = "CTRL|SHIFT|ALT", action = act.DisableDefaultAssignment },
