c2c868225fdea455c9086f87ea821c81fc0b2a96
Antoine Gaudreau Simard <a.simard@multidev.net>
2026-08-31 11:40:48 -0400

    fix(nvim): csv view row highlight

--- Both/Neovim/nvim/lua/plugins/csvview.lua
+++ Both/Neovim/nvim/lua/plugins/csvview.lua
@@ -10,6 +10,33 @@ return {
   config = function(_, opts)
     local csvview = require "csvview"
     csvview.setup(opts)
+    vim.api.nvim_set_hl(0, "CsvViewCursorLine", { bg = "#313244" })
+
+    local cursorline_ns = vim.api.nvim_create_namespace "csvview.cursorline"
+    local cursor_row
+    vim.api.nvim_set_decoration_provider(cursorline_ns, {
+      on_win = function(_, winid, bufnr)
+        if not csvview.is_enabled(bufnr) then
+          return false
+        end
+
+        cursor_row = vim.api.nvim_win_get_cursor(winid)[1] - 1
+      end,
+      on_line = function(_, _, bufnr, row)
+        if row ~= cursor_row then
+          return
+        end
+
+        vim.api.nvim_buf_set_extmark(bufnr, cursorline_ns, row, 0, {
+          end_row = row + 1,
+          ephemeral = true,
+          hl_eol = true,
+          hl_group = "CsvViewCursorLine",
+          hl_mode = "combine",
+          priority = 200,
+        })
+      end,
+    })
 
     vim.api.nvim_create_autocmd("FileType", {
       pattern = { "csv", "tsv" },
