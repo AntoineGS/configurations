727d3cb89bfb94159ffb0f5fe040f1425dba104e
Antoine Gaudreau Simard <a.simard@multidev.net>
2026-09-22 10:20:03 -0400

    fix(shell-picker): pin restore reload hang fix

--- Both/ShellPicker/setup-shell-picker.sh
+++ Both/ShellPicker/setup-shell-picker.sh
@@ -5,7 +5,7 @@ usage() {
   printf 'Usage: %s --check|--apply|--help\n' "${0##*/}"
 }
 
-latest_commit="bbc281740a5cb9e55516babc6c9e99ad88ee5cf6"
+latest_commit="a5072d75672460873207a9a54bc59652d1788cae"
 repository="${SHELL_PICKER_REPOSITORY:-https://github.com/AntoineGS/shell-picker.git}"
 source_dir="${SHELL_PICKER_SOURCE_DIR:-$HOME/gits/shell-picker}"
 binary="${SHELL_PICKER_BINARY:-$HOME/.local/bin/shell-picker}"
